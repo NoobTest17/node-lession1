ca80d19a8bcc9964cf4af028bfa63d4334caf81f
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-02-05 22:46:37 +0300

    lesion 5

---
+++ lession5/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+// // task-1 - читать файл
+// // way 1
+// fs.readFile('t1.txt', 'utf8', (err, data) => {
+//   console.log(data)
+// })
+//
+// const text = fs.readFileSync('t2.txt', 'utf8')
+// console.log(text)
+// console.log('==================')
+
+fs.readdir('./one', (err, data) => {
+   data.forEach(file => {
+     console.log(file + ' \t\t ' + path.extname(file) + '\t\t' + fs.statSync('one/'+file).size + 'B');
+   })
+})
+
+fs.writeFile('one/newFile.txt', 'Good', (err) => {
+  if (err) console.log(err);
+});
